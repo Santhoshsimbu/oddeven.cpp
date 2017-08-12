0d860df7a1bfa6786e8cea89fd3108bd88d80103
Santhoshsimbu <30343927+Santhoshsimbu@users.noreply.github.com>
2017-08-12 16:51:27 +0530

    Create odev.cpp

---
+++ odev.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int number;
+clrscr();
+cout<<"enter the number":
+cin>>number;
+if(number%2=0)
+cout<<"number is even";
+else
+cout<<"number is odd";
+getch();
+}
